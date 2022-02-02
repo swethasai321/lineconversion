dd69daf5fa834ca12d6fb529b8abb2e948eabdde
swethasai321 <96300123+swethasai321@users.noreply.github.com>
2022-02-02 20:40:07 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,45 @@
+package lineconversion;
+
+import java.util.Scanner;
+
+public class length {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double x1,x2,y1,y2,x3,y3,x4,y4;
+		int line1,line2;
+		
+		//first line
+		System.out.println("enter x1&y1 values of first point");
+		x1=scanner.nextDouble();
+		y1=scanner.nextDouble();
+		
+		System.out.println("enter x2&y2 values of second point");
+		x2=scanner.nextDouble();
+		y2=scanner.nextDouble();
+		line1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("Distance of first line = "+line1);
+		//second line
+		System.out.println("enter x3&y3 values of first point");
+		x3=scanner.nextDouble();
+		y3=scanner.nextDouble();
+		
+		System.out.println("enter x4&y4 values of second point");
+		x4=scanner.nextDouble();
+		y4=scanner.nextDouble();
+		line2=(int)Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+		
+	System.out.println("Distance of second line = "+line2);
+	//system.out.println(line1.equals(line2));
+	if (line1 == line2) {
+		System.out.println("both lines are equal");
+	}
+	else {
+		System.out.println("lines are not equal");
+		
+	}
+			
+	
+		
+	}
+
+}
